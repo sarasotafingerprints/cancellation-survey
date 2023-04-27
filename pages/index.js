98410826c84d457e82992315f45fb1bf5f35bba0
sarasotafingerprints <mike@sarasotafingerprints.com>
2023-04-27 12:27:17 -0400

    Update index.js
    fixed url param name

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import { Widget } from '@typeform/embed-react'
 
 export default function Home() {
   return (
-    <Widget id="rCc1QwtY" style={{ width: '100%', height: '100vh' }} className="my-form" inlineOnMobile={true} transitiveSearchParams={["first_name","url"]}/>
+    <Widget id="rCc1QwtY" style={{ width: '100%', height: '100vh' }} className="my-form" inlineOnMobile={true} transitiveSearchParams={["first_name","calendly_url"]}/>
   )
 }
