bb2471fc393d2350eaab4a9726ee1fb467a23644
sarasotafingerprints <mike@sarasotafingerprints.com>
2023-04-27 13:54:18 -0400

    Update index.js
    use url shortenter instead

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import { Widget } from '@typeform/embed-react'
 
 export default function Home() {
   return (
-    <Widget id="rCc1QwtY" style={{ width: '100%', height: '100vh' }} className="my-form" inlineOnMobile={true} transitiveSearchParams={["first_name","last_name","email"]}/>
+    <Widget id="rCc1QwtY" style={{ width: '100%', height: '100vh' }} className="my-form" inlineOnMobile={true} transitiveSearchParams={["first_name","url"]}/>
   )
 }
